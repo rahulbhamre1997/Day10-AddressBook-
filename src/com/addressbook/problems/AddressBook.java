0e9c98eb54f617ca2831cd4e0eb714a580091a4b
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-03-29 10:08:52 +0530

    Welcome message

---
+++ src/com/addressbook/problems/AddressBook.java
@@ -0,0 +1,7 @@
+package com.addressbook.problems;
+
+public class AddressBook {
+	public static void main(String[] args) {
+		System.out.println("welcome to address book ");
+	}
+}
---
+++ src/com/addressbook/problems/MainClass.java
@@ -0,0 +1,5 @@
+package com.addressbook.problems;
+
+public class MainClass {
+
+}
